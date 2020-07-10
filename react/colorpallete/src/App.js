dc23174ee440b0453d36dcde41c52463f774f354
cp5g15 <chittawat.pourpongpan@tesco.com>
2020-07-10 15:21:12 +0100

    first commit

---
+++ react/colorpallete/src/App.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+import Pallete from './Pallete';
+import seedColors from './seedColors'
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Pallete {...seedColors[4]} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/colorpallete/src/Pallete.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Pallete extends Component {
+    render(){
+        return(
+           <div className="Palette">
+               {/* Navbar */}
+               <div className="Pallete-colors"></div>
+               {/* Colorboxes */}
+               {/* footer */}
+           </div>
+        )
+    }
+}
+
+export default Pallete
